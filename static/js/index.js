0020777221716597139398043755b714441bfa1e
Scott Ferguson <scottwferg@gmail.com>
2013-01-11 10:45:21 -0600

    Minor cleanup

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,5 @@
 var socket = io.connect(window.location.hostname);
 
-socket.on('news', function (data) {
-    console.log(data);
-    socket.emit('my other event', { my: 'data' });
-});
-
 socket.on('players', function(data) {
     $('table#player-list').html(ich.header_row({}));
 
