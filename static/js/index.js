3225998b66ebaf7ba721dc91fc02c783da6397fc
Scott Ferguson <scottwferg@gmail.com>
2013-01-15 23:58:44 -0600

    Improved layout

--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,7 @@
 var socket = io.connect(window.location.hostname);
 
 socket.on('players', function(data) {
-    $('table#player-list').html(ich.header_row({}));
+    $('tbody#player-list').html('');
 
     for (var i in data.players) {
         console.log(data.players[i]);
@@ -10,6 +10,6 @@ socket.on('players', function(data) {
         data.players[i].y = data.players[i].y.toFixed(2);
         data.players[i].z = data.players[i].z.toFixed(2);
 
-        $('table#player-list').append(ich.player_row(data.players[i]));
+        $('tbody#player-list').append(ich.player_row(data.players[i]));
     }
 });
