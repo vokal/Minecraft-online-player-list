0cfb8dede4776838102e54d81303187c0dc68128
Scott Ferguson <scottwferg@gmail.com>
2013-01-15 23:42:00 -0600

    Limit location coordinates to 2 decimal points

--- static/js/index.js
+++ static/js/index.js
@@ -5,6 +5,11 @@ socket.on('players', function(data) {
 
     for (var i in data.players) {
         console.log(data.players[i]);
+
+        data.players[i].x = data.players[i].x.toFixed(2);
+        data.players[i].y = data.players[i].y.toFixed(2);
+        data.players[i].z = data.players[i].z.toFixed(2);
+
         $('table#player-list').append(ich.player_row(data.players[i]));
     }
 });
