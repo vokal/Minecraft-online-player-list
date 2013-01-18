627af52d9bd26a888c828a66da2f7385df675ace
Scott Ferguson <scott.ferguson@vokalinteractive.com>
2013-01-18 13:10:13 -0600

    Revert "Revert "Add location persistence""
    This reverts commit 99506dd7da89b0f386191335e4c28e7f06fe4c77.

--- server.js
+++ server.js
@@ -1,13 +1,43 @@
 var express = require('express');
+var Db = require('mongodb').Db;
+var Server = require('mongodb').Server;
 
 var app = express(express.logger());
 var server = require('http').createServer(app)
 var io = require('socket.io').listen(server);
 
+var mongo_uri = process.env.MONGO_ADDRESS || "localhost"
+var mongo_port = process.env.MONGO_PORT || 27017;
+var mongo_db = process.env.MONGO_DB || "mapserve";
+
+var location_update_interval = process.env.UPDATE_INTERVAL || 10;
+
 app.use(express.bodyParser());
 
 app.configure(function() {
     app.use(express.static(__dirname + '/static'));
+});
+
+var mongo_server = new Server(mongo_uri, mongo_port, { autoreconnect: true })
+var db = new Db(mongo_db, mongo_server, { native_parser:false });
+
+var location_update_counter = location_update_interval;
+
+db.open(function(err, db) {
+    if (!err) {
+        console.log("Connected to 'mapserve' database");
+        db.collection('player_locations', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'player_locations' collection doesn't exist.");
+            }
+        });
+
+        db.collection('deathpoint_locations', {safe:true}, function(err, collection) {
+            if (err) {
+                console.log("The 'deathpoint_locations' collection doesn't exist.");
+            }
+        });
+    }
 });
 
 var players = {
@@ -19,6 +49,23 @@ app.get('/', function(req, res) {
     res.render('index.jade', {});
 });
 
+app.get('/locations/:player', function(req, res) {
+    var name = req.param('player');
+
+    db.collection('player_locations', function(err, player_locations) {
+        player_locations.find({'player': name}).toArray(function(err, pl) {
+            db.collection('deathpoint_locations', function(err, deathpoint_locations) {
+                deathpoint_locations.find({'player': name}).toArray(function(err, dl) {
+                    res.json({
+                        'player_locations': pl,
+                        'deathpoint_locations': dl,
+                    });
+                });
+            });
+        });
+    });
+});
+
 app.post('/update', function(req, res) {
     console.log(req.body);
     players = req.body
@@ -26,17 +73,31 @@ app.post('/update', function(req, res) {
     res.send('OKAY');
 });
 
-var port = process.env.PORT || 5000;
-server.listen(port, function() {
-    console.log("Listening on " + port);
-});
+function updateLocations(data) {
+    db.collection('player_locations', function(err, player_locations) {
+        db.collection('deathpoint_locations', function(err, deathpoint_locations) {
+            player_locations.insert(data.players);
+            deathpoint_locations.insert(data.death_points);
+        });
+    });
+}
 
 io.sockets.on('connection', function (socket) {
     socket.emit('players', players);
 
     socket.on('update', function (data) {
-        console.log("Incoming -> ");
-        console.log(data);
         io.sockets.emit('players', data);
+
+        if (location_update_counter == 0) {
+            updateLocations(data);
+            location_update_counter == location_update_interval;
+        } else {
+            location_update_counter--;
+        }
     });
+});
+
+var port = process.env.PORT || 5000;
+server.listen(port, function() {
+    console.log("Listening on " + port);
 });
