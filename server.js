eb5b348b863f2c2e586fb37a2092c59f6ba111b2
Scott Ferguson <scottwferg@gmail.com>
2013-01-27 11:33:34 -0600

    Fix case where updates would happen twice a second, separate locations and deathpoint APIs, add unique constraint to data

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ var mongo_uri = process.env.MONGO_ADDRESS || "localhost"
 var mongo_port = process.env.MONGO_PORT || 27017;
 var mongo_db = process.env.MONGO_DB || "mapserve";
 
-var location_update_interval = process.env.UPDATE_INTERVAL || 10;
+var location_update_interval = process.env.UPDATE_INTERVAL || 120;
 
 app.use(express.bodyParser());
 
@@ -32,19 +32,36 @@ app.get('/', function(req, res) {
     res.render('index.jade', {});
 });
 
+app.get('/locations', function(req, res) {
+    res.json(players);
+});
+
 app.get('/locations/:player', function(req, res) {
     var name = req.param('player');
 
+    var query = {'player': name};
+    if (req.param('t')) {
+        query['timestamp'] = {$gte: Number(req.param('t'))};
+    }
+
     db.collection('player_locations', function(err, player_locations) {
-        player_locations.find({'player': name}).toArray(function(err, pl) {
-            db.collection('deathpoint_locations', function(err, deathpoint_locations) {
-                deathpoint_locations.find({'player': name}).toArray(function(err, dl) {
-                    res.json({
-                        'player_locations': pl,
-                        'deathpoint_locations': dl,
-                    });
-                });
-            });
+        player_locations.find(query).toArray(function(err, pl) {
+            res.json(pl);
+        });
+    });
+});
+
+app.get('/deathpoints/:player', function(req, res) {
+    var name = req.param('player');
+
+    var query = {'player': name};
+    if (req.param('t')) {
+        query['timestamp'] = {$gte: Number(req.param('t'))};
+    }
+
+    db.collection('deathpoint_locations', function(err, deathpoint_locations) {
+        deathpoint_locations.find(query).toArray(function(err, dl) {
+            res.json(dl);
         });
     });
 });
@@ -58,12 +75,18 @@ app.post('/update', function(req, res) {
 
 function updateLocations(data) {
     db.collection('player_locations', function(err, player_locations) {
-        db.collection('deathpoint_locations', function(err, deathpoint_locations) {
-            player_locations.insert(data.players);
-            deathpoint_locations.insert(data.death_points);
+        player_locations.ensureIndex({timestamp: 1}, {unique:true}, function(err, indexName) {
+            db.collection('deathpoint_locations', function(err, deathpoint_locations) {
+                deathpoint_locations.ensureIndex({timestamp: 1}, {unique:true}, function(err, indexName) {
+                    player_locations.insert(data.players);
+                    deathpoint_locations.insert(data.death_points);
+                });
+            });
         });
     });
 }
+
+io.set('log level', 1);
 
 io.sockets.on('connection', function (socket) {
     socket.emit('players', players);
@@ -72,8 +95,9 @@ io.sockets.on('connection', function (socket) {
         io.sockets.emit('players', data);
 
         if (location_update_counter == 0) {
+            console.log('Updating locations');
             updateLocations(data);
-            location_update_counter == location_update_interval;
+            location_update_counter = location_update_interval;
         } else {
             location_update_counter--;
         }
@@ -83,32 +107,46 @@ io.sockets.on('connection', function (socket) {
 // MongoHQ can be slow to connect, so we need to hold off on starting
 // the webserver until that connection is established
 db.open(function(err, db) {
+    if (err) {
+        console.log(err);
+        return;
+    }
+
     var username = process.env.MONGO_USERNAME || '';
     var password = process.env.MONGO_PASSWORD || '';
-    db.authenticate(username, password, function(err, conn) {
-        if (err) {
-            console.log(err);
-            return;
-        }
 
-        console.log("Database opened");
-
-        db.collection('player_locations', {safe:true}, function(err, collection) {
+    if (username && password) {
+        db.authenticate(username, password, function(err, conn) {
             if (err) {
-                console.log("The 'player_locations' collection doesn't exist.");
+                console.log(err);
+                return;
             }
-        });
 
-        db.collection('deathpoint_locations', {safe:true}, function(err, collection) {
-            if (err) {
-                console.log("The 'deathpoint_locations' collection doesn't exist.");
-            }
-        });
+            console.log("Database opened");
 
-        // Now that we've connected to the database, let's start the webserver
-        var port = process.env.PORT || 5000;
-        server.listen(port, function() {
-            console.log("Listening on " + port);
+            runserver();
         });
-    });
+    } else {
+        runserver();
+    }
 });
+
+var runserver = function() {
+    db.collection('player_locations', {safe:true}, function(err, collection) {
+        if (err) {
+            console.log("The 'player_locations' collection doesn't exist.");
+        }
+    });
+
+    db.collection('deathpoint_locations', {safe:true}, function(err, collection) {
+        if (err) {
+            console.log("The 'deathpoint_locations' collection doesn't exist.");
+        }
+    });
+
+    // Now that we've connected to the database, let's start the webserver
+    var port = process.env.PORT || 5000;
+    server.listen(port, function() {
+        console.log("Listening on " + port);
+    });
+}
